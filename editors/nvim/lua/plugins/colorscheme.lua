f7017a29a5b08042494c1a76f79c534410ccf922
Yordy Carmona <yordy.carmona8@gmail.com>
2023-12-15 10:30:35 -0300

    add nvim directory with my configuration

---
+++ editors/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,50 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    opts = {
+      transparent_background = true,
+      flavour = "mocha",
+    },
+    integrations = {
+      cmp = true,
+      gitsigns = true,
+      nvimtree = true,
+      treesitter = true,
+      notify = false,
+      mini = {
+        enabled = true,
+        indentscope_color = "",
+      },
+      -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+    },
+  },
+  {
+    "miikanissi/modus-themes.nvim",
+    name = "modus",
+    priority = 1000,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    name = "kanagawa",
+    priority = 1000,
+  },
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000,
+    opts = {
+      style = "deep",
+      -- transparent = true,
+    },
+  },
+  {
+    "xiyaowong/transparent.nvim",
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
