6fcba5d3af21d96a242d315d836c6424afe06f42
yordycg <yordy.carmona8@gmail.com>
2024-03-22 22:10:01 -0300

    update: alias

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -3,29 +3,38 @@ alias sudo='sudo '
 
 alias ..="cd .."
 alias ...="cd ../.."
+alias ....="cd ../../.."
+
 # alias ll="ls -l"
 # alias la="ls -la"
-# Install exa/github
-alias ll="exa -l --icons"
-alias la="exa -la --icons"
-alias lla="exa -la --group --icons"
-alias lt="exa --tree --icons" # install tree
+
+# Exa | install exa
+alias ll="exa -l --icons --git -a"
+alias la="exa -la --icons --git"
+alias lla="exa -la --group --icons --git"
+alias lt="exa --tree --level=3 --long --icons --git" # install tree
+
 alias ~="cd ~"
 alias dotfiles='cd $DOTFILES_PATH'
 
 # Git
+alias g="git"
+alias gcl="git clone"
 alias gaa="git add -A"
-alias gc='$DOTLY_PATH/bin/dot git commit'
+alias gc="git commit"
 alias gca="git add --all && git commit --amend --no-edit"
 alias gco="git checkout"
-alias gd='$DOTLY_PATH/bin/dot git pretty-diff'
+alias gd="git pretty-diff"
 alias gs="git status -sb"
 alias gf="git fetch --all -p"
 alias gps="git push"
 alias gpsf="git push --force"
 alias gpl="git pull --rebase --autostash"
+alias gpp="git pull && git push"
+alias gm="git merge"
 alias gb="git branch"
-alias gl='$DOTLY_PATH/bin/dot git pretty-log'
+alias gl="git pretty-log"
+alias gg="git log --graph --pretty=format:'%C(bold)%h%Creset%C(magenta)%d%Creset %s %C(yellow)<%an> %C(cyan)(%cr)%Creset' --abbrev-commit --date=relative"
 
 # Utils
 alias k='kill -9'
@@ -35,10 +44,13 @@ alias o.='open .'
 alias up='dot package update_all'
 
 alias setup="mkdir \"$1\" && cd \"$1\""
-alias zshc="nvim ~/.zshrc"
-alias atyc="nvim ~/.config/alacritty/alacritty.toml"
-alias so="source ~/.zshrc"
-alias cl="clear"
+alias ali="$EDITOR $DOTFILES/shell/aliases.sh"
+alias zshrc="$EDITOR ~/.zshrc"
+alias bashrc="$EDITOR ~/.bashrc"
+alias al="$EDITOR ~/.config/alacritty/alacritty.toml"
+alias so="source ~/.bashrc"
+alias t="tmux"
+alias c="clear"
 alias x="exit"
 alias bat="batcat" # instalar desde 'bat/github'
 
@@ -50,6 +62,10 @@ alias search="fzf --preview 'bat --color=always --style=numbers --line-range=:50
 alias vim="nvim"
 alias vi="nvim"
 alias v="nvim ."
+
+# Directories
+alias dot="cd $DOTFILES"
+alias repos="cd $REPOS"
 
 # WSL
 alias win="cd /mnt/c/Users/yordyPC" # change a yordyWIN
---
+++ shell/fzf.sh
@@ -0,0 +1,4 @@
+# Setup fzf
+# ----------
+# source /home/linuxbrew/.linuxbrew/Cellar/fzf/0.46.1/shell/key-bindings.bash
+# source /home/linuxbrew/.linuxbrew/Cellar/fzf/0.46.1/shell/completion.bash
